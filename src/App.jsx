4a2573371c3e32478ce1f21164c5b06a5355f3bc
chayanon12 <chayanonolo@gmail.com>
2024-01-29 10:13:47 +0700

    add something

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>{import.meta.env.VITE_API}</h1>
+      <h1>{import.meta.env.VITE_API}</h1>
+      <h2>chayanon</h2>
+      <h2>chayanon</h2>
+      <h2>chayanon</h2>
+      <h2>chayanon</h2>
+      <h2>chayanon</h2>
+      <h2>chayanon</h2>
+      <h2>chayanon</h2>
+      <h2>chayanon</h2>
+      <h2>chayanon</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
